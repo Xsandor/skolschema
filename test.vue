9c72a94039031ce00eb9aeacedd9680e3109726f
Alexander Schmidt <alex@karlborgselkontroll.se>
2019-08-15 21:55:58 +0200

    init

---
+++ test.vue
@@ -0,0 +1,24 @@
+<template>
+  <FullCalendar
+    defaultView="timeGridWeek"
+    :plugins="calendarPlugins" 
+    :slotEventOverlap="false"
+  />
+</template>
+
+<script>
+import FullCalendar from '@fullcalendar/vue'
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarPlugins: [ timeGridPlugin ]
+    }
+  }
+}
+
+</script>
